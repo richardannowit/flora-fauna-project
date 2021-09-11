83a49849c8ae30bda2c8f5a5785cdc9054804fa6
maitrongnhan001 <maitrongnhan.001@gmail.com>
2021-09-11 21:33:27 +0700

    added router

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,28 @@
 import './App.scss';
 import Header from './Components/Header/Header';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from './Components/Pages/Home';
+import Categories from './Components/Pages/Categories';
+import Products from './Components/Pages/Products';
+import SearchProduct from './Components/Pages/SearchProduct';
+import LoginPage from './Components/Pages/LoginPage';
+import ContractPage from './Components/Pages/ContractPage';
 
 function App() {
   return (
-    <div className="App">
-      <Header></Header>
-    </div>
+    <>
+      <Router>
+        <Header></Header>
+        <Switch>
+          <Route path='/' exact component={Home}></Route>
+          <Route path='/categories' component={Categories}></Route>
+          <Route path='/products' component={Products}></Route>
+          <Route path='/search' component={SearchProduct}></Route>
+          <Route path='/login' component={LoginPage}></Route>
+          <Route path='/contract' component={ContractPage}></Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
