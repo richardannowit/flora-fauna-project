49bc0d69f95aa4c30af50d95a8cb84fd0164b2bc
maitrongnhan001 <maitrongnhan.001@gmail.com>
2021-09-11 21:34:27 +0700

    add footer component

---
+++ client/src/Components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+
+    render() {
+        return (
+            <section className="footer">
+                <div className="container text-center">
+                    <p>All rights reserved. Designed By <a href="/" className="pink">Mai Trong Nhan</a></p>
+                </div>
+            </section>
+        );
+    }
+}
+
+export default Footer;
