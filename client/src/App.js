8b5ad4ba98fc82b0fe67933198dfe2308cdc1aee
maitrongnhan001 <maitrongnhan.001@gmail.com>
2021-10-02 19:50:01 +0700

    update path login

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.scss';
+import LoginPage from './Components/Customer/Pages/LoginPage';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Customer from './Components/Customer/Customer';
 
@@ -10,14 +11,8 @@ class App extends Component {
       <Router>
         <Switch>
           <Route path='/' exact component={Customer}></Route>
-          <Route path='/categories' component={Customer}></Route>
-          <Route path='/products' component={Customer}></Route>
-          <Route path='/search' component={Customer}></Route>
-          <Route path='/login' exact component={Customer}></Route>
-          <Route path='/contract' exact component={Customer}></Route>
-          <Route path='/order' exact component={Customer}></Route>
-          <Route path='/productDetails' exact component={Customer}></Route>
           <Route path='/admin'></Route>
+          <Route path='/login' component={LoginPage}></Route>
         </Switch>
       </Router>
     </>
--- client/src/Components/Customer/Customer.js
+++ client/src/Components/Customer/Customer.js
@@ -5,7 +5,6 @@ import Home from './Pages/Home';
 import Categories from './Pages/Categories';
 import Products from './Pages/Products';
 import SearchProduct from './Pages/SearchProduct';
-import LoginPage from './Pages/LoginPage';
 import ContractPage from './Pages/ContractPage';
 import OrderPage from './Pages/OrderPage';
 import ProductDetailsPage from './Pages/ProductDetailsPage';
@@ -91,7 +90,6 @@ class App extends Component {
                 );
               }}
             ></Route>
-            <Route path='/login' component={LoginPage}></Route>
             <Route path='/contract' component={ContractPage}></Route>
             <Route path='/order' component={OrderPage}></Route>
             <Route path='/productDetails' component={ProductDetailsPage}></Route>
