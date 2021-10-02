092668788555d706f9e902594732131268b023b4
maitrongnhan001 <maitrongnhan.001@gmail.com>
2021-10-02 20:25:12 +0700

    update path login

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,18 @@
 import React, { Component } from 'react';
 import './App.scss';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import LoginPage from './Components/Customer/Pages/LoginPage';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Customer from './Components/Customer/Customer';
 import Admin from './Components/Admin/Admin';
 class App extends Component {
   render() {
     return (
       <Router>
+        <Switch>
           <Route path='/' exact component={Customer}></Route>
           <Route path='/admin' component={Admin}></Route>
-          <Route path='/categories' component={Customer}></Route>
-          <Route path='/products' component={Customer}></Route>
-          <Route path='/search' component={Customer}></Route>
-          <Route path='/login' exact component={Customer}></Route>
-          <Route path='/contract' exact component={Customer}></Route>
-          <Route path='/order' exact component={Customer}></Route>
-          <Route path='/productDetails' exact component={Customer}></Route>
+          <Route path='/login' component={LoginPage}></Route>
+        </Switch>
       </Router>
     );
   }
