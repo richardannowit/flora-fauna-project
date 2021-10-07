af3e86a258da2cb1030bc070c86d3cf29bd0a2c5
namvt002 <viettrung0601@gmail.com>
2021-10-07 20:34:37 +0700

    insert into order, update viewall orders

--- models/Food.js
+++ models/Food.js
@@ -3,7 +3,7 @@ const { connection } = require("../config/database");
 
 module.exports.viewFood = function viewFood(){
     return new Promise((resolve, reject)=>{
-        connection.query("SELECT foods.id, foods.food_name, foods.price, foods.description, categories.category_name FROM foods INNER JOIN categories ON foods.category_id = categories.id ORDER BY foods.id;", function(error, result){
+        connection.query("SELECT foods.id, foods.food_name, foods.price,foods.image_name, foods.description, categories.category_name FROM foods INNER JOIN categories ON foods.category_id = categories.id ORDER BY foods.id;", function(error, result){
             if (error) {
                 reject(error);
             }else{
