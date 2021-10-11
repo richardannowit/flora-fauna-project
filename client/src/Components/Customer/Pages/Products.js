3ffa436c8e6cf30f22d604b3eb422e32e0f9d0a6
maitrongnhan001 <maitrongnhan.001@gmail.com>
2021-10-11 19:33:02 +0700

    update function search

--- client/src/Components/Customer/Pages/Products.js
+++ client/src/Components/Customer/Pages/Products.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { getProducts,getProductsByIdCategory } from '../API/Connect-API';
+import { getProducts, getProductsByName, getProductsByIdCategory } from '../API/Connect-API';
 import Product from '../Product/Product';
 import Search from '../Search/Search';
 class Products extends Component {
@@ -7,11 +7,24 @@ class Products extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            ContentSearch: "",
             Products: []
         };
     }
 
     async componentDidMount() {
+        //run when use function search in order page
+        //get list product
+        const name_product = this.props.ContentSearch;
+        if (name_product) {
+            const products = await getProductsByName(name_product);
+            this.setState({
+                ContentSearch: name_product,
+                Products: products.data
+            });
+            return;
+        }
+
         //get list product by id category
         const id_category = this.props.IdCategory;
         if (id_category) {
@@ -22,6 +35,7 @@ class Products extends Component {
             //die process
             return;
         }
+
         //get list product limit 6 element
         const limit = 6;
         const products = await getProducts(limit);
@@ -30,11 +44,25 @@ class Products extends Component {
         });
     }
 
+    async componentDidUpdate (prevProps) {
+        //run when use function search in here
+        //get list product
+        const name_product = this.props.ContentSearch;
+        const products = await getProductsByName(name_product);
+        if (prevProps.ContentSearch !== this.props.ContentSearch) {
+            this.setState({
+                ContentSearch: name_product,
+                Products: products.data
+            });
+        }
+    }
+
     render() {
         return (
             <>
                 <Search
                 HandleSearch={this.props.HandleSearch}
+                ContentSearch={this.state.ContentSearch}
                 ></Search>
                 <Product 
                 Products={this.state.Products}
