ee7dca75018970b7e5cf72d30e37642f161204b0
Tran Dang Khoa <khoab1805879@student.ctu.edu.vn>
2021-10-14 20:55:19 +0700

    Create and update category

--- routes/category.js
+++ routes/category.js
@@ -3,12 +3,27 @@ const express = require('express')
 const categoryController = require('../controllers/CategoryController')
 
 const router = express.Router()
+const multer = require('multer')
+const path = require('path');
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/uploads')
+    },
+    filename: function (req, file, cb) {
+        cb(null, 'Category_' + Date.now() + '_' + file.originalname)
+    }
+})
+var upload = multer({ storage: storage })
 
 
 router.get('/', categoryController.viewCategories);
 
 //url = /api/categories/categories_name/:search
-router.get('/categories_name/:search',categoryController.findCategory);
+router.get('/categories_name/:search', categoryController.findCategory);
+
+router.post('/', upload.single('file'), categoryController.create);
+router.put('/:id', upload.single('file'), categoryController.update);
 
 router.delete('/:id', categoryController.delete);
 
